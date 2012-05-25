ea2665cf5a3e7ebfb83c8f405c5b44afebc73d33
Luke Baker <bakerlu@missouri.edu>
2012-05-25 09:35:40 -0500

    Added documenation strings

--- parsepst.py
+++ parsepst.py
@@ -6,6 +6,7 @@ from sys import exit
 infile = '/home/bakerlu/pst_output/2011'
 
 def load(file):
+	"""Load json words file"""
 	with open(file, 'r') as f:
 		return json.loads(f.read())
 
@@ -13,6 +14,15 @@ words = load('words.json')
 regex = re.compile(r'%s' % '|'.join(words))
 
 def parse(file):
+	"""
+	Opens pst file
+	Looks for matches against the compiled regex (words list)
+		-Example, r'failure|mistake|problem'
+	
+	@param file Word list
+	@return matches Dictionary of <word match>:<count>
+	"""
+
 	with open(file, 'r') as f:
 		out = f.read()
 
@@ -26,18 +36,23 @@ def parse(file):
 	else:
 		return matches
 
-def classify(result_dict):
+def classify(matches):
+	"""
+	Select one of three font sizes to represent word occurance in the PST file.
+	Output is HTML
+
+	@param result_dict Dictionary of word matches
+	"""
 	#CSS font sizes
 	font_sizes = [10,20,30]
-	max_num = max(result_dict.values())
+	max_num = max(matches.values())
 
 	#Interval
 	step = max_num / len(range(0,2))
 
-	for word, count in result_dict.items():
+	for word, count in matches.items():
 		font_size_index = count / step
 		print '<p style="font-size:%dpx">%s - %d</p>' % (font_sizes[font_size_index], word, count)
-		#print '<p style="font-size:%dpx">%s</p>' % (font_sizes[font_size_index], word)
 	
 if __name__ == '__main__':
 	try:
